7faaac89bcadd68f5c324503b22c4659caa928f7
Vikash2398 <160607273+Vikash2398@users.noreply.github.com>
2024-02-21 20:58:42 +0530

    Update avax4token.sol

--- avax4token.sol
+++ avax4token.sol
@@ -1,41 +1,45 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SpiderManGame is Ownable {
-    address public spiderMan;
-    uint256 public numPlayers;
-    bool public gameInProgress;
+contract SpiderManGame is ERC20Burnable, Ownable {
+    mapping(address => uint256) public killCount;
+    mapping(address => bool) public rewardsRedeemed;
 
-    event PlayerJoined(address player);
-    event VillainDefeated(address villain);
+    event PlayerKilled(address indexed player, uint256 enemies);
+    event RewardsIssued(address indexed player, uint256 rewards);
+    event RewardsRedeemed(address indexed player);
 
-    constructor(address initialOwner) Ownable(initialOwner) {
-        spiderMan = initialOwner;
-        gameInProgress = false;
-        numPlayers = 0;
+    constructor() ERC20("SpiderManToken", "SPM") {
+        _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
     }
 
-    function joinGame() public {
-        require(!gameInProgress, "Game is already in progress");
-        numPlayers++;
-        emit PlayerJoined(msg.sender);
+    function collectKillCount(address player, uint256 enemies) public onlyOwner {
+        killCount[player] += enemies;
+        emit PlayerKilled(player, enemies);
     }
 
-    function startGame() public onlyOwner {
-        require(numPlayers >= 1, "Minimum one player required to start the game");
-        gameInProgress = true;
+    function issueRewards(address player, uint256 rewards) public onlyOwner {
+        rewardsRedeemed[player] = false;
+        _mint(player, rewards);
+        emit RewardsIssued(player, rewards);
     }
 
-    function defeatVillain() public onlyOwner {
-        require(gameInProgress, "No game in progress");
-        emit VillainDefeated(msg.sender);
-        gameInProgress = false;
+    function redeemRewards() public {
+        require(!rewardsRedeemed[msg.sender], "Rewards already redeemed");
+        rewardsRedeemed[msg.sender] = true;
+        emit RewardsRedeemed(msg.sender);
     }
 
-    function endGame() public onlyOwner {
-        gameInProgress = false;
+    function checkBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function transferTokens(address recipient, uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(msg.sender, recipient, amount);
     }
 }
